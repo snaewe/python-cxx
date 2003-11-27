c3cb6c647de30206c83ff7633d4c15577bfee0ce
Barry Alan Scott <barry-scott at users.sourceforge.net>
2003-11-27 14:43:14 +0000

    Add apply for keywords
    git-svn-id: https://cxx.svn.sourceforge.net/svnroot/cxx/trunk@41 cb2f5747-f50d-0410-b093-b27f9c363ba5

--- CXX/CXX/Objects.hxx
+++ CXX/CXX/Objects.hxx
@@ -2425,6 +2425,12 @@ namespace Py
 			return asObject(PyObject_CallObject(ptr(), args.ptr()));
 			}
 
+		// Call with keywords
+		Object apply(const Tuple& args, const Dict& kw) const
+			{
+			return asObject( PyEval_CallObjectWithKeywords( ptr(), args.ptr(), kw.ptr() ) );
+			}
+
 		Object apply(PyObject* pargs = 0) const
 			{
 			return apply (Tuple(pargs));
