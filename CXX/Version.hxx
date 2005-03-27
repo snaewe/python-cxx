1b8914027a01c1c0d0e34e91bb434bcd9c91ddf8
Barry Alan Scott <barry-scott at users.sourceforge.net>
2005-03-27 21:14:21 +0000

    Add missing py23 linux example make file
    Allow users to check the version of PyCXX using
    macros defined in Version.hpp

---
+++ CXX/Version.hxx
@@ -0,0 +1,11 @@
+//
+//  CXX/Version.hxx
+//
+#ifndef __PyCXX_version_hxx__
+#define __PyCXX_version_hxx__
+
+#define PYCXX_VERSION_MAJOR 5
+#define PYCXX_VERSION_MINOR 3
+#define PYCXX_VERSION_PATCH 3
+#define PYCXX_VERSION ((PYCXX_VERSION_MAJOR<<16)|(PYCXX_VERSION_MINOR<<8)|(PYCXX_VERSION_PATCH))
+#endif
